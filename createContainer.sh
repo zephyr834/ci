f806a55770ca40631055c8e0c3cad355747c5562
shiv <sprasad0603@gmail.com>
2015-10-16 13:05:28 -0700

    Modified createContainer to use local submodule directory for all helper scripts

--- createContainer.sh
+++ createContainer.sh
@@ -2,6 +2,7 @@
 set -e
 
 BASEDIR=$(readlink -f $(dirname $0))
+SCRIPT_DIR=./img-scripts
 
 # Add common variables.
 source ${BASEDIR}/config
@@ -9,23 +10,23 @@ source ${BASEDIR}/config.default
 
 # Create Nexus server.
 if [ ${#NEXUS_WEBURL} -eq 0 ]; then
-    source ~/nexus-docker/createNexus.sh
+    source ${SCRIPT_DIR}/nexus-docker/createNexus.sh
 fi
 
 # Create OpenLDAP server.
 if [ ${#SLAPD_DOMAIN} -gt 0 -a ${#SLAPD_PASSWORD} -gt 0 ]; then
-    source ~/openldap-docker/createOpenLDAP.sh
+    source ${SCRIPT_DIR}/openldap-docker/createOpenLDAP.sh
 fi
 
 # Create Gerrit server container.
-source ~/gerrit-docker/createGerrit.sh
+source ${SCRIPT_DIR}/gerrit-docker/createGerrit.sh
 
 # Create Jenkins server container.
-source ~/jenkins-docker/createJenkins.sh
+source ${SCRIPT_DIR}/jenkins-docker/createJenkins.sh
 
 # Create Redmine server container.
-source ~/redmine-docker/createRedmine.sh
+source ${SCRIPT_DIR}/redmine-docker/createRedmine.sh
 
 # Create Nginx proxy server container.
-source ~/nginx-docker/createNginx.sh
+source ${SCRIPT_DIR}/nginx-docker/createNginx.sh
 
