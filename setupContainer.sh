dc674cb85181265b3737a4da323443caf8fde850
georgi <hipapi@gmail.com>
2015-11-05 12:20:28 -0800

    jenkins build works

--- setupContainer.sh
+++ setupContainer.sh
@@ -16,7 +16,7 @@ ${SCRIPT_DIR}/gerrit-docker/addGerritUser.sh ${GERRIT_WEBURL} ${GERRIT_ADMIN_UID
 
 #Integrate Jenkins with Gerrit.
 echo ">>>> Setup Jenkins."
-${SCRIPT_DIR}/jenkins-docker/setupJenkins.sh ${GERRIT_ADMIN_UID} ${GERRIT_ADMIN_EMAIL} ${SSH_KEY_PATH} ${LDAP_OU_ACCOUNTS} ${JENKINS_NAME} ${GERRIT_NAME} ${GERRIT_SSH_HOST} ${GERRIT_WEBURL} ${JENKINS_WEBURL} ${NEXUS_REPO} ${LDAP_NAME} ${LDAP_VOLUME} ${SLAPD_DOMAIN}
+${SCRIPT_DIR}/jenkins-docker/setupJenkins.sh ${GERRIT_ADMIN_UID} ${GERRIT_ADMIN_EMAIL} ${SSH_KEY_PATH} ${LDAP_ACCOUNTBASE} ${JENKINS_NAME} ${GERRIT_NAME} ${GERRIT_SSH_HOST} ${GERRIT_WEBURL} ${JENKINS_WEBURL}  ${LDAP_NAME} ${LDAP_VOLUME} ${SLAPD_DOMAIN} ${NEXUS_REPO}
 
 #Integrate Redmine with Openldap and import init data.
 echo ">>>> Setup Redmine."
