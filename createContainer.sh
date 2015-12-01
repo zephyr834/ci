8ecbf5d71419c11ac636cb272f59440de55aed64
shiv <sprasad0603@gmail.com>
2015-12-01 15:35:53 -0800

    Moved nexus creation after LDAP to ensure linking takes place and stopped using 'call_create_script' function for Nexus

--- createContainer.sh
+++ createContainer.sh
@@ -23,12 +23,6 @@ call_create_script() {
 source ${BASEDIR}/config
 source ${BASEDIR}/config.default
 
-# Create Nexus server.
-if [ ${#NEXUS_WEBURL} -eq 0 ]; then
-#    source ${SCRIPT_DIR}/nexus-docker/createNexus.sh
-    call_create_script ${NEXUS_DIR} createNexus.sh
-fi
-
 # Create OpenLDAP server.
 if [ ${#SLAPD_DOMAIN} -gt 0 -a ${#SLAPD_PASSWORD} -gt 0 ]; then
     ${SCRIPT_DIR}/openldap-docker/createOpenLDAP.sh ${SLAPD_PASSWORD} ${SLAPD_DOMAIN} ${GERRIT_ADMIN_UID} ${GERRIT_ADMIN_PWD} ${GERRIT_ADMIN_EMAIL} 
@@ -45,6 +39,13 @@ ${SCRIPT_DIR}/jenkins-docker/createJenkins.sh ${JENKINS_NAME} ${JENKINS_VOLUME} 
 # Create Redmine server container.
 ${SCRIPT_DIR}/redmine-docker/createRedmine.sh ${PG_REDMINE_NAME} ${POSTGRES_IMAGE_NAME} ${REDMINE_NAME} ${REDMINE_IMAGE_NAME} ${REDMINE_VOLUME} ${GERRIT_VOLUME} ${LDAP_NAME} ${LDAP_ACCOUNTBASE}
 #call_create_script ${REDMINE_DIR} createRedmine.sh
+
+# Create Nexus server.
+if [ ${#NEXUS_WEBURL} -eq 0 ]; then
+#    source ${SCRIPT_DIR}/nexus-docker/createNexus.sh
+    ${SCRIPT_DIR}/nexus-docker/createNexus.sh ${NEXUS_NAME} ${NEXUS_VOLUME} ${NEXUS_IMAGE_NAME} ${LDAP_NAME}
+#    call_create_script ${NEXUS_DIR} createNexus.sh
+fi
 
 # Create Nginx proxy server container.
 ${SCRIPT_DIR}/nginx-docker/createNginx.sh ${HOST_NAME} ${GERRIT_NAME} ${JENKINS_NAME} ${REDMINE_NAME} ${NEXUS_NAME} ${NGINX_IMAGE_NAME} ${NGINX_NAME} ${LDAP_NAME} ${SLAPD_DOMAIN} ${SLAPD_PASSWORD}
